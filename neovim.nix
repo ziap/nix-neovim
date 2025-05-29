2a38b4cb28dedc41a6c5643826cc26ab26f2637b
Zap <67962871+ziap@users.noreply.github.com>
2025-05-29 21:54:25 +0700

    Move wrapper code to an external C file

--- neovim.nix
+++ neovim.nix
@@ -73,56 +73,18 @@
   neovim-custom = pkgs.stdenv.mkDerivation {
     pname = "neovim-custom";
     version = "1.0.0";
-    src = pkgs.writeText "wrapper.c" /* c */ ''
-      #include <stdio.h>
-      #include <stdlib.h>
-      #include <string.h>
-      #include <unistd.h>
-
-      int main(int argc, char **argv) {
-        const char extra_paths[] = "${lib.makeBinPath extraPackages}";
-        const char command[] = "${pkgs.neovim-unwrapped}/bin/nvim";
-
-        char option[] = "-u";
-        char config_file[] = "${init-lua}";
-
-        const char *path = getenv("PATH");
-        if (path == NULL) path = "";
-
-        size_t path_len = strlen(path);
-        size_t buf_size = path_len + sizeof(extra_paths) + 1;
-        size_t args_size = sizeof(char*) * (argc + 3);
-        if (args_size > buf_size) buf_size = args_size;
-
-        void *buf = malloc(buf_size);
-        if (buf == NULL) {
-          perror("Failed to allocate memory");
-          return 1;
-        }
-
-        char *updated = buf;
-        memcpy(updated, path, path_len);
-        updated[path_len] = ':';
-        memcpy(updated + path_len + 1, extra_paths, sizeof(extra_paths));
-
-        if (setenv("PATH", updated, 1) != 0) {
-          perror("Failed to set virtual environment");
-          free(updated);
-          return 1;
-        }
-
-        char **args = buf;
-        memcpy(args, argv, sizeof(char*) * argc);
-        args[argc + 0] = option;
-        args[argc + 1] = config_file;
-        args[argc + 2] = NULL;
-
-        execv(command, args);
-        perror("Failed to execute command");
-        free(buf);
-        return 1;
-      }
-    '';
+    src = pkgs.writeText "wrapper.c" (builtins.replaceStrings
+      [
+        "<extra_paths>"
+        "<command>"
+        "<config_file>"
+      ]
+      [
+        (lib.makeBinPath extraPackages)
+        "${pkgs.neovim-unwrapped}/bin/nvim"
+        "${init-lua}"
+      ]
+      (builtins.readFile ./wrapper.c));
     dontUnpack = true;
     buildPhase = ''
       cc -o nvim -O2 -s $src
---
+++ wrapper.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+  const char extra_paths[] = "<extra_paths>";
+  const char command[] = "<command>";
+
+  char option[] = "-u";
+  char config_file[] = "<config_file>";
+
+  const char *path = getenv("PATH");
+  if (path == NULL) path = "";
+  size_t path_len = strlen(path);
+
+  size_t buf_size = path_len + sizeof(extra_paths) + 1;
+  size_t args_size = sizeof(char*) * (argc + 3);
+  if (args_size > buf_size) buf_size = args_size;
+  void *buf = malloc(buf_size);
+  if (buf == NULL) {
+    perror("Failed to allocate memory");
+    return 1;
+  }
+
+  {
+    char *updated = buf;
+    memcpy(updated, path, path_len);
+    updated[path_len] = ':';
+    memcpy(updated + path_len + 1, extra_paths, sizeof(extra_paths));
+
+    if (setenv("PATH", updated, 1) != 0) {
+      perror("Failed to set virtual environment");
+      free(updated);
+      return 1;
+    }
+  }
+
+  {
+    char **args = buf;
+    memcpy(args, argv, sizeof(char*) * argc);
+    args[argc + 0] = option;
+    args[argc + 1] = config_file;
+    args[argc + 2] = NULL;
+
+    execv(command, args);
+    perror("Failed to execute command");
+  }
+
+  free(buf);
+  return 1;
+}
